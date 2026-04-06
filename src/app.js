338df313e02e21b5d196216652e55c8935201964
tranduyha9111 <tranduy.ha911@gmail.com>
2026-04-06 12:36:09 +0700

    update port

--- src/app.js
+++ src/app.js
@@ -23,4 +23,6 @@ const authRoutes = require("./routes/authRoutes");
 app.use("/api/auth", authRoutes);
 
 // server
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
